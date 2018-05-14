86d8fd76225b40e0744eb29f31b285679b8d5ba8
gianksp <gianksp@gmail.com>
2018-05-14 22:22:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,3 +9,22 @@ async function startProgram() {
 		await delay(1.0);
 	}
 }
+
+async function onCollision() {
+	stopRoll();
+	setMainLed({ r: 255, g: 0, b: 0 });
+	await speak("Collision", false);
+	setHeading((getHeading() + 180));
+	await delay(0.5);
+	setMainLed({ r: 255, g: 22, b: 255 });
+	setSpeed(100);
+}
+registerEvent(EventType.onCollision, onCollision);
+
+async function onFreefall() {
+	setMainLed({ r: 0, g: 255, b: 0 });
+	speak("freefall", false);
+	await delay(0.5);
+}
+registerEvent(EventType.onFreefall, onFreefall);
+
